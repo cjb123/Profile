5af53b525e57b8af9a63edb88089593c142bca30
Chinmoy <chinmoy.borah@apollo247.org>
2023-02-25 15:22:36 +0530

    animations added, 1st flow completed

--- src/components/introTexts.jsx
+++ src/components/introTexts.jsx
@@ -3,12 +3,15 @@ import '../styles/introTexts.scss'
 
 const IntroTexts = ({ coverOpen }) => {
     return (
-        <div className="intro-texts">
-            <div className={`introducing ${coverOpen ? 'intro-animation intro-animation-2': ''}`}>Introducing..</div>
-            <div className={'name'}>
-                <div className="kantora">KANTORA</div>
-                <img src='kantara.jpeg' width='200px' height='200px'/>
+        <div className="">
+            { coverOpen && 
+            <div className="intro-texts">
+            <div className={`introducing ${coverOpen ? 'intro-animation': ''}`}>Introducing..</div>
+            <div className='intro-user-name'>
+                Chinmoy Jyoti Borah
             </div>
+            </div>
+            }
         </div>
     )
 }
